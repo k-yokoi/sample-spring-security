6b8112934faaa49eaf269f165c557fb5106ae82a
Kazuki Yokoi <kazuki.yokoi11@gmail.com>
2020-05-24 18:59:42 +0900

    first commit

---
+++ src/main/java/net/kyokoi/sample/spring/security/chat/SampleSpringSecurityChatApplication.java
@@ -0,0 +1,13 @@
+package net.kyokoi.sample.spring.security.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringSecurityChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleSpringSecurityChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/kyokoi/sample/spring/security/chat/SampleSpringSecurityChatApplicationTests.java
@@ -0,0 +1,13 @@
+package net.kyokoi.sample.spring.security.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringSecurityChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
